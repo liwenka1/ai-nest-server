97756a7ee11a6b24e153228b61cb5ef0aea00c24
unknown <2020583117@qq.com>
2025-04-04 23:13:50 +0800

    fix: 修复使用环境变量作为auth模块的隐私变量

--- src/auth/constants.ts
+++ src/auth/constants.ts
@@ -1,8 +1,8 @@
 import { SetMetadata } from '@nestjs/common';
 
-export const IS_PUBLIC_KEY = 'isPublic';
+export const IS_PUBLIC_KEY = process.env.IS_PUBLIC_KEY;
 export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
 
 export const jwtConstants = {
-  secret: 'DO NOT USE THIS VALUE. INSTEAD, CREATE A COMPLEX SECRET AND KEEP IT SAFE OUTSIDE OF THE SOURCE CODE.'
+  secret: process.env.JWT_SECRET
 };
