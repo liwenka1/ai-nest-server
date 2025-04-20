9514b12a9294dd2072b50f845f78b512e8a5c848
unknown <2020583117@qq.com>
2025-04-20 23:49:50 +0800

    fix: 代码退回，保证可以正常使用

--- src/prisma/prisma.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PrismaService } from './prisma.service';
-
-@Module({
-  providers: [PrismaService],
-  exports: [PrismaService] // 必须导出
-})
-export class PrismaModule {}
--- src/videos/videos.module.ts
+++ src/videos/videos.module.ts
@@ -3,13 +3,10 @@ import { VideosController } from './videos.controller';
 import { VideosService } from './videos.service';
 import { HttpModule } from '@nestjs/axios';
 import { HttpClientService } from '../common/http-client.service';
-import { UserModule } from '../user/user.module'; // 正确导入
-import { PrismaModule } from '../prisma/prisma.module'; // 正确导入
-import { UserService } from '../user/user.service';
 
 @Module({
-  imports: [HttpModule, UserModule, PrismaModule], // 通过 imports 注入依赖
+  imports: [HttpModule],
   controllers: [VideosController],
-  providers: [VideosService, HttpClientService, UserService]
+  providers: [VideosService, HttpClientService]
 })
 export class VideosModule {}
